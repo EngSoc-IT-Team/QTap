50384ee2eb44e38f23491e92710368335e3678b2
Carson Cook <cook.carson38@gmail.com>
2017-06-06 20:21:08 -0400

    created xml for about activity

---
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/AboutActivity.java
@@ -0,0 +1,11 @@
+package com.example.alex.qtapandroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Carson on 06/06/2017.
+ * In options menu, contains information about app
+ */
+public class AboutActivity extends AppCompatActivity {
+
+}
--- QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/MainTabActivity.java
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/MainTabActivity.java
@@ -94,17 +94,14 @@ public class MainTabActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        switch(item.getItemId()) {
+            case R.id.settings:
             Intent settings = new Intent(MainTabActivity.this, SettingsActivity.class);
             startActivity(settings);
+            case R.id.about:
+                Intent about = new Intent(MainTabActivity.this,AboutActivity.class);
+                startActivity(about);
         }
-
         return false;
     }
 
