722cc8fcdc0b1356b3e2fb841e8cc97eafdd54da
Carson Cook <cook.carson38@gmail.com>
2017-06-06 20:40:22 -0400

    cleaned up setttings activity code

--- QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/SettingsActivity.java
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/SettingsActivity.java
@@ -1,30 +1,12 @@
 package com.example.alex.qtapandroid.activities;
 
 
-import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.support.v7.app.ActionBar;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.webkit.CookieManager;
-import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
@@ -39,71 +21,42 @@ import com.example.alex.qtapandroid.common.database.users.UserManager;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
+ * Activity for the settings. Can see NetID, time since calendar was last synced and can logout here
  */
 public class SettingsActivity extends AppCompatActivity {
 
-    private void ClearData() {
-//        android.webkit.CookieManager cookieManager = CookieManager.getInstance();
+    private void ClearData(View v) {
         CookieManager.getInstance().removeAllCookies(null);
         CookieManager.getInstance().flush();
-
         WebView web = new WebView(getApplicationContext());
         web.clearFormData();
         web.clearHistory();
         web.clearCache(true);
 
-        // Todo: launch the settings activity with a bundle or flag to clear webView form data so that NetIDs do not show up in the username box
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            cookieManager.removeAllCookies(new ValueCallback<Boolean>() {
-//                // a callback which is executed when the cookies have been removed
-//                @Override
-//                public void onReceiveValue(Boolean aBoolean) {
-//                    Log.d("TEST", "Cookie removed: " + aBoolean);
-//                }
-//            });
-//        } else cookieManager.removeAllCookie();
-
-
+        v.getContext().deleteDatabase(SqlStringStatements.PHONE_DATABASE_NAME);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_page);
+
         Button logout = (Button) findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Context context = v.getContext();
-                context.deleteDatabase(SqlStringStatements.PHONE_DATABASE_NAME);
-
-                ClearData();
-                new WebView(getApplicationContext()).clearCache(true);
-                Intent intent = new Intent(SettingsActivity.this , LoginActivity.class);
+                ClearData(v);
+                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
-
             }
         });
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        //String userEmail = preferences.getString("UserEmail", "defaultStringIfNothingFound");
+
         String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-//        String userName = preferences.getString("UserName", "USERNAME");
         UserManager mUserManager = new UserManager(this.getApplicationContext());
         ArrayList<User> user = mUserManager.getTable();
-        String uNetID = user.get(0).getNetid();
-
+        String uNetID = user.get(0).getNetid(); //only ever one user in database
         TextView netID = (TextView) findViewById(R.id.netID);
         TextView date = (TextView) findViewById(R.id.login_date);
         date.setText(currentDateTimeString);
@@ -115,5 +68,4 @@ public class SettingsActivity extends AppCompatActivity {
         super.onPause();
         DatabaseAccessor.getDatabase().close(); //ensure only one database connection is ever open
     }
-
 }
