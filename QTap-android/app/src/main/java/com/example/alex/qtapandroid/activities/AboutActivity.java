a5a980b44aedd042f93bbf4dc96844dedb8cdadf
Carson Cook <cook.carson38@gmail.com>
2017-06-28 19:37:46 -0400

    added optiones menu to activities without

--- QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/AboutActivity.java
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/AboutActivity.java
@@ -1,8 +1,11 @@
 package com.example.alex.qtapandroid.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.alex.qtapandroid.R;
 
@@ -16,5 +19,23 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.about_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent settings = new Intent(AboutActivity.this, SettingsActivity.class);
+                startActivity(settings);
+                break;
+        }
+        return false;
     }
 }
--- QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/SettingsActivity.java
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/SettingsActivity.java
@@ -4,6 +4,8 @@ package com.example.alex.qtapandroid.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.WebView;
@@ -64,6 +66,24 @@ public class SettingsActivity extends AppCompatActivity {
         TextView date = (TextView) findViewById(R.id.login_date);
         date.setText(currentDateTimeString);
         netID.setText(uNetID);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Intent about = new Intent(SettingsActivity.this, AboutActivity.class);
+                startActivity(about);
+                break;
+        }
+        return false;
     }
 
     @Override
